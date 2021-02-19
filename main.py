9a1c11c8df70e598fc0eccd7b7a182b0a45f1e76
vlad <DegaDega001@mail.ru>
2021-02-19 23:44:35 +0300

    add array

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# print('Print number :')
-# b = int(input())
-#
-# a = 5
-# print("a = " + str(a ** b))
-#
-# if b > a : print(b ** a)
-# elif b < a : print(a ** b)
 print("hello world!!!")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+string = ["My first program with python3 !", "Mw first program with docker !"]
+
+print(string[0])
+print(string[1])
