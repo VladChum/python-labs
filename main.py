3ab780d44dafde4523354a55a35c9a4733164484
vlad <DegaDega001@mail.ru>
2021-02-21 02:09:22 +0300

    add logger and main

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 # This is a sample Python script.
-print("hello world!!!")
 
-string = ["My first program with python3 !", "Mw first program with docker !"]
+import logging
+import sys
 
-print(string[0])
-print(string[1])
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_formatter = logging.Formatter("%(message)s")
+console_handler.setFormatter(console_formatter)
+console_handler.setLevel(logging.INFO)
+
+logger.addHandler(console_handler)
+
+
+def print_spring():
+    logger.info("hello world!!!")
+    string = ["My first program with python3 !", "Mw first program with docker !"]
+    logger.info(string[0])
+    logger.info(string[1])
+
+
+def main():
+    print_spring()
+
+
+if __name__ == "__main__":
+    main()
